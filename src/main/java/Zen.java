c4ba69ad34bbaae552929de4cfafb1858b4e4afa
Unknownflow <clarencelau88@gmail.com>
2026-08-14 13:56:36 +0800

    Rename chatbot from Duke to Zen

---
+++ src/main/java/Zen.java
@@ -0,0 +1,10 @@
+public class Zen {
+    public static void main(String[] args) {
+        String banner = "  ______              \n" +
+                        " |__  /___  _ __      \n" +
+                        "   / // _ \\| '_ \\   \n" +
+                        "  / /|  __/| | | |    \n" +
+                        " /____\\___||_| |_|   \n";
+        System.out.println(banner);
+    }
+}
