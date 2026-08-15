011cc5405c8a27da687e249821ba19d5bae39a2a
Unknownflow <clarencelau88@gmail.com>
2026-08-15 10:32:35 +0800

    Replace if with switch statements and update banner

--- src/main/java/Zen.java
+++ src/main/java/Zen.java
@@ -7,11 +7,13 @@ public class Zen {
     private static final String NAME = "Zen";
     private static final String INDENTATION = "    ";
     private static final String INDENTED_DIVIDER = INDENTATION + DIVIDER;
-    private static final String BANNER = "     ______              \n" +
-                                         "    |__  /___  _ __      \n" +
-                                         "      / // _ \\| '_ \\   \n" +
-                                         "     / /|  __/| | | |    \n" +
-                                         "    /____\\___||_| |_|";
+    private static final String BANNER = """
+                 ______             \s
+                |__  /___  _ __     \s
+                  / // _ \\| '_ \\  \s
+                 / /|  __/| | | |   \s
+                /____\\___||_| |_|\
+            """;
 
     /**
      * Prints an echo of the input string, wrapped with dividers
@@ -77,24 +79,29 @@ public class Zen {
             }
 
             System.out.println(INDENTED_DIVIDER);
-            if (command.equals("list")) {
-                listTasks(tasks);
-            } else if (command.equals("mark")){
-                int idx = scanner.nextInt();
-                // array is 0-indexed, hence it is idx - 1
-                Task task = tasks.get(idx - 1);
-                markTaskDone(task);
-            } else if (command.equals("unmark")) {
-                int idx = scanner.nextInt();
-                // array is 0-indexed, hence it is idx - 1
-                Task task = tasks.get(idx - 1);
-                markTaskNotDone(task);
-            } else {
-                // if there is no command given, add task to the List and echo it to the user
-                String userInput = command + scanner.nextLine();
-                Task newTask = new Task(userInput);
-                tasks.add(newTask);
-                echo(userInput);
+
+            // AI-assisted: AI suggested using switch instead of if statements
+            switch (command) {
+                case "list" -> listTasks(tasks);
+                case "mark" -> {
+                    int idx = scanner.nextInt();
+                    // array is 0-indexed, hence it is idx - 1
+                    Task task = tasks.get(idx - 1);
+                    markTaskDone(task);
+                }
+                case "unmark" -> {
+                    int idx = scanner.nextInt();
+                    // array is 0-indexed, hence it is idx - 1
+                    Task task = tasks.get(idx - 1);
+                    markTaskNotDone(task);
+                }
+                default -> {
+                    // if there is no command given, add task to the List and echo it to the user
+                    String userInput = command + scanner.nextLine();
+                    Task newTask = new Task(userInput);
+                    tasks.add(newTask);
+                    echo(userInput);
+                }
             }
 
             System.out.println(INDENTED_DIVIDER);
