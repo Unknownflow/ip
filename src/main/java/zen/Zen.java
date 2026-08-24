a50c2668d8e2685ca9ffe2e01ba0240f649811e5
Unknownflow <clarencelau88@gmail.com>
2026-08-24 14:19:11 +0800

    Add Javadocs for Zen

--- src/main/java/zen/Zen.java
+++ src/main/java/zen/Zen.java
@@ -6,12 +6,18 @@ import zen.storage.Storage;
 import zen.task.TaskList;
 import zen.ui.Ui;
 
+/** Start point of the Zen chatbot. */
 public class Zen {
 
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
 
+    /**
+     * Creates the chatbot and loads any tasks stored at the given path.
+     *
+     * @param filePath location of the task list file
+     */
     public Zen(String filePath) {
         ui = new Ui();
         storage = new Storage(filePath);
@@ -24,6 +30,7 @@ public class Zen {
         }
     }
 
+    /** Runs the command loop until the user enters the exit command. */
     public void run() {
         ui.printGreeting();
         boolean isExit = false;
@@ -43,7 +50,7 @@ public class Zen {
         }
     }
 
-    public static void main(String[] args) throws ZenException {
+    public static void main(String[] args) {
         new Zen("data/task_list.txt").run();
     }
 }
--- src/main/java/zen/ZenException.java
+++ src/main/java/zen/ZenException.java
@@ -1,7 +1,13 @@
 package zen;
 
+/** Represents a user-facing error that Zen can display and recover from. */
 public class ZenException extends Exception {
-    public ZenException(String s) {
-        super(s);
+    /**
+     * Creates an exception containing a user-facing error message.
+     *
+     * @param message explanation of the error
+     */
+    public ZenException(String message) {
+        super(message);
     }
 }
